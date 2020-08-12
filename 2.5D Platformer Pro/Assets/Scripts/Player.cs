ee0236bbec34fd67ad3bb4e13baa5eed52e95afa
Michiel Burger <michielburger0@gmail.com>
2020-08-12 16:27:42 +1200

    Physics Based Character Controller
    Physics Based Character Controller section implemented

---
+++ 2.5D Platformer Pro/Assets/Scripts/Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5.0f;
+    [SerializeField]
+    private float _gravity = 1.0f;
+    [SerializeField]
+    private float _jumpHeight = 20.0f;
+
+    private CharacterController _controller;
+    private float _yVelocity;
+    private bool _canDoubleJump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(horizontalInput, 0, 0);
+        Vector3 velocity = direction * _speed;
+
+        //ifgrounded
+        //do nothing
+        //else
+        //apply gravity
+
+        //velocity.y - _gravity
+
+        if (_controller.isGrounded == true)
+        {
+            //if hit space key
+            //jump
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = _jumpHeight;
+                _canDoubleJump = true;
+            }
+
+        } else
+        {
+            //check for double jump
+            //current _yvelocity += jumpheight
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                if(_canDoubleJump == true)
+                {
+                    _yVelocity += _jumpHeight;
+                    _canDoubleJump = false;
+                }
+            }
+
+            _yVelocity -= _gravity;
+        }
+
+        velocity.y = _yVelocity;
+
+        _controller.Move(velocity * Time.deltaTime);
+    }
+}
